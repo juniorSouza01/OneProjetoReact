ebea7d38d4100945e25b8fe06fdc8d3f1ec0e6c8
juniorSouza01 <juniorthesouza017@gmail.com>
2023-01-06 16:14:08 -0300

    alteração

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Routes from "./src/Routes";
 export default function App() {
   return (
     <>
-      <StatussBar barStyle={"auto"} />
+      <StatusBar barStyle={"auto"} />
       <Routes />
     </>
   );
