2b1f0e866c9ab1749b905a14fa7c7127d6a79b11
juniorSouza01 <juniorthesouza017@gmail.com>
2023-01-09 13:35:30 -0300

    alteração

--- src/Pages/Home/teste.jsx
+++ src/Pages/Home/teste.jsx
@@ -3,7 +3,6 @@ import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 
 const UselessTextInput = () => {
   const [text, onChangeText] = React.useState("Useless Text");
-  const [number, onChangeNumber] = React.useState(null);
 
   return (
     <SafeAreaView>
@@ -11,13 +10,6 @@ const UselessTextInput = () => {
         style={styles.input}
         onChangeText={onChangeText}
         value={text}
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="Digitar"
-        keyboardType="text"
       />
     </SafeAreaView>
   );
